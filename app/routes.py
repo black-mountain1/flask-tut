d31e4429b8c8445fd54e3c50a03cc03422766a2a
Paul Trouton <pault_25@hotmail.com>
2021-01-03 10:22:16 +0000

    working on microblog

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +18,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}")
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
