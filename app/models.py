1bbedd1b90d54398bb17a8113933b5875c86a401
Paul Trouton <pault_25@hotmail.com>
2021-01-04 07:49:18 +0000

    working on login

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 from app import db
+from app import login
 
 
 class User(UserMixin, db.Model):
@@ -30,3 +31,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.body}>'
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
