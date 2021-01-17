152480f0b28a6a7d168317978859d75d25f55efd
Paul Trouton <pault_25@hotmail.com>
2021-01-17 11:42:24 +0000

    displaying posts on landing page

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,8 @@ from datetime import datetime
 from app import app
 from app import db
 from app.forms import RegistrationForm, EditProfileForm, EmptyForm
-from app.forms import LoginForm
-from app.models import User
+from app.forms import LoginForm, PostForm
+from app.models import User, Post
 
 
 @app.before_request
@@ -19,22 +19,20 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'Paul'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home Page', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home Page', form=form,
+                           posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
